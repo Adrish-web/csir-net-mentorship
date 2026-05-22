beae4b455ebdc7e638048210382b34701cdc2194
Adrish Ghosh <adrish@Adrishs-MacBook-Air.local>
2026-05-22 18:07:29 +0530

    fixed images

--- src/components/NotesPreview.jsx
+++ src/components/NotesPreview.jsx
@@ -17,7 +17,7 @@ const NOTES_DATA = [
     title: "Cell Cycle & Apoptosis",
     unit: "Unit 2: Cellular Organization",
     pages: 14,
-    previewUrl: "./logos/apoptosis.jpeg"
+    previewUrl: "../logos/apoptosis.jpeg"
   },
   {
     id: 3,
